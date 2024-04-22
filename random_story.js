094ff5c9b03d332d32ae9209bcebc34d36cd8968
Kioho <146140500+Kioho@users.noreply.github.com>
2024-04-22 04:45:21 +0200

    Update random_story.js

--- random_story.js
+++ random_story.js
@@ -4,17 +4,19 @@
 
 
 function createStory () {
-    const firstPart = ['dog', 'cat', 'rabbit', 'squirrel', 'deer'];
-    const secondPart = ['loved', 'hated', 'enjoyed', 'disliked'];
-    const thirdPart = ['run', 'jump', 'climb', 'crawl', 'sneak'];
-    const fourthPart = ['hangout', 'hiding place', 'forests', 'beach'];
-    const getRandom = num => Math.floor(Math.random() * num);
-    const getPart = storyPartArr => {
-        return storyPartArr[getRandom(storyPartArr.length)];
-    };
-    const parts = [getPart(firstPart), getPart(secondPart), getPart(thirdPart), getPart(fourthPart)];
-    const randomStory = `There once was a ${parts[0]} that ${parts[1]} to ${parts[2]} all the way to the ${parts[3]}`;
-    return randomStory;
+    return new Promise((resolve, reject) => {
+        const firstPart = ['dog', 'cat', 'rabbit', 'squirrel', 'deer'];
+        const secondPart = ['loved', 'hated', 'enjoyed', 'disliked'];
+        const thirdPart = ['run', 'jump', 'climb', 'crawl', 'sneak'];
+        const fourthPart = ['hangout', 'hiding place', 'forests', 'beach'];
+        const getRandom = num => Math.floor(Math.random() * num);
+        const getPart = storyPartArr => {
+            return storyPartArr[getRandom(storyPartArr.length)];
+        };
+        const parts = [getPart(firstPart), getPart(secondPart), getPart(thirdPart), getPart(fourthPart)];
+        const randomStory = `There once was a ${parts[0]} that ${parts[1]} to ${parts[2]} all the way to the ${parts[3]}`;
+        resolve(randomStory);
+    });
 }
 
 async function myFunction() {
