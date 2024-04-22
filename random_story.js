d90174ccb2cbdf9098ad8ec1ffb35f72dff6e403
Kioho <146140500+Kioho@users.noreply.github.com>
2024-04-22 04:20:18 +0200

    Update random_story.js

--- random_story.js
+++ random_story.js
@@ -1,31 +1,25 @@
 //const {asciiArt} = require('./ASCII_Animals');
 
 
-const firstPart = ['dog', 'cat', 'rabbit', 'squirrel', 'deer'];
-const secondPart = ['loved', 'hated', 'enjoyed', 'disliked'];
-const thirdPart = ['run', 'jump', 'climb', 'crawl', 'sneak'];
-const fourthPart = ['hangout', 'hiding place', 'forests', 'beach'];
 
-const getRandom = num => Math.floor(Math.random() * num);
-
-const getPart = storyPartArr => {
-    return storyPartArr[getRandom(storyPartArr.length)];
-};
 
 function createStory () {
-    const parts = [ getPart(firstPart), getPart(secondPart), getPart(thirdPart), getPart(fourthPart)];
-    //console.log(asciiArt.animals[parts[0]]);
+    const firstPart = ['dog', 'cat', 'rabbit', 'squirrel', 'deer'];
+    const secondPart = ['loved', 'hated', 'enjoyed', 'disliked'];
+    const thirdPart = ['run', 'jump', 'climb', 'crawl', 'sneak'];
+    const fourthPart = ['hangout', 'hiding place', 'forests', 'beach'];
+    const getRandom = num => Math.floor(Math.random() * num);
+    const getPart = storyPartArr => {
+        return storyPartArr[getRandom(storyPartArr.length)];
+    };
+    const parts = [getPart(firstPart), getPart(secondPart), getPart(thirdPart), getPart(fourthPart)];
     const randomStory = `There once was a ${parts[0]} that ${parts[1]} to ${parts[2]} all the way to the ${parts[3]}`;
-    //const storyAnimal = asciiArt.animals[parts[0]];
-    //const returnList = [randomStory, storyAnimal];
-    //return returnList;
     return randomStory;
 }
 
 async function myFunction() {
     let text = await createStory();
     document.getElementById("randomStory").innerHTML = text;
-    document.getElementById("storyAnimal").innerHTML = storyAnimal;
 }
 
 //module.exports = { createStory }
