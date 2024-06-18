9269e2772382ad9f8bdf7827b5434a78b524af75
Kioho <146140500+Kioho@users.noreply.github.com>
2024-06-18 16:37:09 +0200

    Update random_story.js

--- random_story.js
+++ random_story.js
@@ -14,13 +14,14 @@ function createStory () {
             return storyPartArr[getRandom(storyPartArr.length)];
         };
         const parts = [getPart(firstPart), getPart(secondPart), getPart(thirdPart), getPart(fourthPart)];
-        const randomStory = `There once was a ${parts[0]} that ${parts[1]} to ${parts[2]} all the way to the ${parts[3]}`;
+        const randomStory = [`There once was a ${parts[0]} that ${parts[1]} to ${parts[2]} all the way to the ${parts[3]}`], [parts[0]];
         resolve(randomStory);
     });
 }
 
 async function myFunction() {
     let text = await createStory();
-    document.getElementById("randomStory").innerHTML = text;
+    document.getElementById("randomStory").innerHTML = text[0];
+    document.getElementById("randomASCII_pic").innerHTML = text[1];
 }
 //module.exports = { createStory }
